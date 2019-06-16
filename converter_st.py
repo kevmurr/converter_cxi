47942deec0e30f3495fd83d774ce1e1a2d3fa307
kevmurr <31069141+kevmurr@users.noreply.github.com>
2019-06-16 15:17:10 +0200

    First version without syntax errors

--- converter_st.py
+++ converter_st.py
@@ -12,6 +12,7 @@ import scipy.constants as sc
 #Additional parameters
 dbeam_cut_range=10
 roi_size=10
+year="2019"
 
 
 #input arguments
@@ -33,8 +34,20 @@ scan_name="Scan_{}".format(scanno)
 #Paths
 logdir = "/gpfs/cfel/cxi/labs/MLL-Sigray/scan-logs/"
 scanssdir = "/gpfs/cfel/cxi/labs/MLL-Sigray/Scans/"
+savedir=""
+#savedir= "/gpfs/cfel/cxi/labs/MLL-Sigray/Processed/{0}/".format(year)
 mask="/gpfs/cfel/cxi/labs/MLL-Sigray/mask/lambda_mask1.h5"
 maskpath="/data"
+
+#Old permanent config things
+pxsize_x=55*10**-6
+pxsize_y=55*10**-6
+pxsize_z=0
+unit_scanmotor=10**-3
+unitvector_f=np.array((-1,0,0))
+unitvector_s=np.array((0,-1,0))
+
+
 
 
 #Load mask
@@ -43,20 +56,27 @@ mask=maskfile[maskpath][()].astype(np.int)
 
 #Load log and read tings
 lognam="{0}{1}.log".format(logdir,scan_name)
-logfile=open(,"r")
-for line in log:
+logfile=open(lognam,"r")
+scanmotor=None
+for line in logfile:
     if line.beginswith("# Points count"):
         N_points=int(line.split(":")[1])
-    if line.beginswith():
+    if line.beginswith("# Device:") and line.endswith("Scanner")==False and line.endswith("Lambda")==False:
+        scanmotor=str(line.split(":")[1][1:])
+if scanmotor==None:
+    print("WARNING: No proper scanmotor detected!")
+else:
+    print("Scanmotor is {}".format(scanmotor))
+
+if scanmotor=="SAMX":
+    orientation="h"
+elif scanmotor=="SAMY":
+    orientation="v"
+else:
+    print("Scanmotor is not SAMX or SAMY. Don't know orientation. Assuming h.")
+    orientation="h"
 
 
-def bld_basis(whole_data):
-    basis_vectors=np.zeros((whole_data.shape[0],2,3))
-    pixel_vector=np.array((cf.pxsize_x,cf.pxsize_y,cf.pxsize_z))
-    for i in range(0,whole_data.shape[0],1):
-        basis_vectors[i,0,:]=np.multiply(cf.unitvector_f,pixel_vector)
-        basis_vectors[i,1,:]=np.multiply(cf.unitvector_s,pixel_vector)
-    return(basis_vectors)
 
 
 #Loading the data
@@ -70,102 +90,119 @@ files.sort()
 print("Detected {} files.".format(len(files)))
 if N_points!=len(files):
     print("WARNING! Number of expected files does not match number of detected files.")
-for i in range(0,len(files)):
-    if i==0:
-        example_f=h5.File(ldir+"/"+files[i],"r")
-        if orientation=="v":
+useframes=np.ones((N_points)).astype(np.bool)
+startframe=0
+for i in range(0,N_points):
+    try:
+        if i==startframe:
+            example_f=h5.File(ldir+"/"+files[i],"r")
             example_data=example_f["/entry/instrument/detector/data"][0,:,:]
+            if np.sum(example_data)==0:
+                startframe+=1
+                print("First frame was only zeros")
+                useframes[i]=False
+            else:
+                db_coord=np.unravel_index(np.argmax(np.multiply(mask,example_data)),example_data.shape)
+                print("Direct beam pixel coordinate is {0}.".format(db_coord))
+                roi=(db_coord[0]-int(roi_size/2),db_coord[0]+int(roi_size/2),db_coord[1]-int(roi_size/2),db_coord[1]+int(roi_size/2))
+                print("Startframe is {0}. Creating full data array.".format(i))
+                data_full=np.zeros((N_points,example_data.shape[0],example_data.shape[1]))
         else:
-            example_data = example_f["/entry/instrument/detector/data"][0, :, :]
-        data_full=np.zeros((len(files),example_data.shape[0],example_data.shape[1]))
-    f_now=h5.File(ldir+"/"+files[i],"r")
-    if orientation=="h":
-        data_now=np.sum(f_now["/entry/instrument/detector/data"][0,cf.roi[0]:cf.roi[1],:],axis=0)
-        data_insert = np.zeros_like(example_data)
-        for i1 in range(0,data_insert.shape[0],1):
-            data_insert[i1,:]=data_now
-    else:
-        data_now = np.sum(f_now["/entry/instrument/detector/data"][0, :, cf.roi[2]:cf.roi[3]],axis=1)
-        data_insert = np.zeros_like(example_data)
-        for i1 in range(0, data_insert.shape[1], 1):
-            data_insert[:, i1] = data_now
-    data_full[i,:,:]=data_insert
-#now writing the cxifile
-cxifile=h5.File(i_sdir+"/"+scan_prefix+".cxi","w")
-#now creating the subbranches:
-#entry_1
+            f_now=h5.File(ldir+"/"+files[i],"r")
+            if orientation=="h":
+                data_now=np.sum(f_now["/entry/instrument/detector/data"][0,roi[0]:roi[1],:],axis=0)
+                data_now[(db_coord[1]-int(dbeam_cut_range/2)):(db_coord[1]+int(dbeam_cut_range/2))]=np.zeros_like(data_now[(db_coord[1]-int(dbeam_cut_range/2)):(db_coord[1]+int(dbeam_cut_range/2))])
+                data_insert = np.zeros_like(example_data)
+                for i1 in range(0,data_insert.shape[0],1):
+                    data_insert[i1,:]=data_now
+            else:
+                data_now = np.sum(f_now["/entry/instrument/detector/data"][0, :, roi[2]:roi[3]],axis=1)
+                data_now[(db_coord[0] - int(dbeam_cut_range / 2)):(db_coord[0] + int(dbeam_cut_range / 2))] = np.zeros_like(data_now[(db_coord[0] - int(dbeam_cut_range / 2)):(db_coord[0] + int(dbeam_cut_range / 2))])
+                data_insert = np.zeros_like(example_data)
+                for i1 in range(0, data_insert.shape[1], 1):
+                    data_insert[:, i1] = data_now
+            data_full[i,:,:]=data_insert
+    except (KeyError,OSError):
+        if i==startframe:
+            startframe+=1
+        print("Didnt find file {0} or proper data in file.".format(ldir+"/"+files[i]))
+        useframes[i]=False
+
+#writing the cxifile
+if os.path.isdir(savedir+scan_name)==False:
+    os.mkdir(savedir+scan_name)
+cxifile=h5.File("{0}{1}/{1}.cxi".format(savedir,scan_name),"w")
 entry_1 = cxifile.create_group("entry_1")
-    #data_1
 print("Saving data...")
 data_1 = entry_1.create_group("data_1")
-        #data
-data=data_1.create_dataset("data",data=data_full)
-    #instrument_1
-print("Saving metadata...")
+data=data_1.create_dataset("data",data=data_full[useframes])
 instrument_1=entry_1.create_group("instrument_1")
-        #detector_1
 detector_1=instrument_1.create_group("detector_1")
-                #basis vectors
-basis_vectors_cxi=detector_1.create_dataset("basis_vectors",data=bld_basis(whole_data=data_full))
-detector_dist=float(cf.detector_distance)
-detector_distance_cxi=detector_1.create_dataset("distance",data=detector_dist)
-                #mask
-print("Making mask...")
-mask=np.ones((data_full.shape[1],data_full.shape[2]))
-energy=cf.energy*1000*sc.e
+
+def bld_basis(whole_data,useframes=useframes):
+    basis_vectors=np.zeros((whole_data.shape[0],2,3))
+    pixel_vector=np.array((pxsize_x,pxsize_y,pxsize_z))
+    for i in range(0,whole_data.shape[0],1):
+        basis_vectors[i,0,:]=np.multiply(unitvector_f,pixel_vector)
+        basis_vectors[i,1,:]=np.multiply(unitvector_s,pixel_vector)
+    return(basis_vectors[useframes,:,:])
+
+basis_vectors_cxi=detector_1.create_dataset("basis_vectors",data=bld_basis(whole_data=data_full,useframes=useframes))
+detector_distance_cxi=detector_1.create_dataset("distance",data=float(detector_dist))
 print("Energy is %s J"%energy)
 print("Making wavelength")
 wavelength=(sc.h*sc.c)/energy
 print("Wavelength is %s m" %wavelength)
-############################################
-#Now import the positions
-translation_file=np.loadtxt(fpos,skiprows=24)
-translation_vec=np.zeros((translation_file.shape[0],3))
-if orientation=="v":
-	translation_vec[:,0]=translation_file[:,1]
-else:
-	translation_vec[:,1]=translation_file[:,1]
 
-N_images=data_full.shape[0]
-############################################
-#powder
-print("Makint powder array...")
-if ff_tck==True:
-    f_powder=h5.File(ff_fnam,"r")
-    powderarr=f_powder["/entry/instrument/detector/data"][0,:,:]
+unit_dict={
+" m":1,
+"mm":10**-3,
+"µm":10**-6,
+"nm":10**-9,
+"pm":10**-12,
+}
+
+#Now import the positions
+translation_file=logfile
+i=0
+positions=np.zeros((N_points)).astype("float")
+for line in translation_file and line.beginswith("#")==False:
+    current_entry=line.split(";")[2]
+    if i==0:
+        unit=current_entry[-2:]
+    unit_scaling=float(unit_dict[unit])
+    current_pos=float(current_entry[:-2])*unit_scaling
+    positions[i]=current_pos
+    i+=1
+
+translation_vec=np.zeros((N_points,3))
+if orientation=="v":
+	translation_vec[:,0]=positions
 else:
-    powderarr=np.ones((data_full.shape[1],data_full.shape[2]))
-    powderarr=np.sum(data_full,axis=0)
+	translation_vec[:,1]=positions
+translation_vec=translation_vec[useframes,:]
+
+
+print("Makint powder array...")
+powderarr=np.ones((data_full.shape[1],data_full.shape[2]))
+powderarr=np.sum(data_full[useframes],axis=0)
 print("Making good frames array...")
-good_frames_arr=np.arange(0,N_images,1)
-##############################################
-#looking for the files and the numbers
-##############################################
-			#x_pixel_size
-x_pixel_cxi=detector_1.create_dataset("x_pixel_size",data=cf.pxsize_x)
-y_pixel_cxi=detector_1.create_dataset("y_pixel_size",data=cf.pxsize_y)
-            #source_1
+good_frames_arr=np.arange(0,N_points,1)
+x_pixel_cxi=detector_1.create_dataset("x_pixel_size",data=pxsize_x)
+y_pixel_cxi=detector_1.create_dataset("y_pixel_size",data=pxsize_y)
 source_1=instrument_1.create_group("source_1")
-                #wavelength
 wavelength_cxi=source_1.create_dataset("wavelength",data=wavelength)
-                #energy
 energy_cxi=source_1.create_dataset("energy",data=energy)
-    #sample_3
 sample_2=entry_1.create_group("sample_2")
 sample_3=entry_1.create_group("sample_3")
-        #geometry
 geometry=sample_3.create_group("geometry")
-            #translation
-translation=geometry.create_dataset("translation",data=translation_vec*10**-9)
-#process_2
-process_2=cxifile.create_group("process_2")
-#powder
-powder=process_2.create_dataset("powder",data=powderarr)
-#process_3
-process_3=cxifile.create_group("process_3")
-#good frames
-good_frames=process_3.create_dataset("good_frames",data=good_frames_arr)
-#mask
+translation=geometry.create_dataset("translation",data=translation_vec)
+make_whitefield=cxifile.create_group("make_whitefield")
+whitefield=make_whitefield.create_dataset("whitefield",data=powderarr)
+frame_selector=cxifile.create_group("frame_selector")
+good_frames=frame_selector.create_dataset("good_frames",data=good_frames_arr[useframes])
 mask=mask.astype("bool")
-mask_cxi_3=process_3.create_dataset("mask",data=mask)
+mask_maker=cxifile.create_group("mask_maker")
+mask_cxi_3=mask_maker.create_dataset("mask",data=mask)
 print("Done.")
+cxifile.close()
